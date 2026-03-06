30d905ed59c1f6155aa9cf8ae2d05c758c74aed5
Georgiy Mikhailenko <kislball@yahoo.com>
2026-03-07 00:26:19 +0300

    Refactor tests

--- crates/lambubu_cli/benches/reduction.rs
+++ crates/lambubu_cli/benches/reduction.rs
@@ -8,55 +8,66 @@ fn standard_environment() -> CompoundEnvironment {
     CompoundEnvironment::new(vec![Box::new(ChurchEnvironment)])
 }
 
-fn bench_term<'a, M: Measurement>(
+fn bench_term_order<'a, M: Measurement>(
     group: &mut BenchmarkGroup<'a, M>,
     term_source: &str,
+    strategy_name: &str,
     standard_reducer: fn(Term) -> Term,
     bruijn_reducer: fn(BruijnLevelsTerm) -> BruijnLevelsTerm,
 ) {
     let term = compile_term(term_source, &standard_environment()).unwrap();
     let bruijned: BruijnLevelsTerm = term.clone().into();
 
-    group.bench_function(format!("{term_source} - Bruijn"), |b| {
+    group.bench_function(format!("{strategy_name} - {term_source} - Bruijn"), |b| {
         b.iter(|| {
             let mut bruijned = bruijned.clone();
-            while !bruijned.is_normal_form() {
+            loop {
+                let previous = bruijned.clone();
                 bruijned = bruijn_reducer(bruijned.clone());
+                if previous == bruijned {
+                    break;
+                }
             }
         });
     });
 
-    group.bench_function(format!("{term_source} - Standard"), |b| {
+    group.bench_function(format!("{strategy_name} - {term_source} - Standard"), |b| {
         b.iter(|| {
             let mut term = term.clone();
-            while !term.is_normal_form() {
+            loop {
+                let previous = term.clone();
                 term = standard_reducer(term.clone());
+                if previous == term {
+                    break;
+                }
             }
         });
     });
 }
 
-fn ao_bench(c: &mut Criterion) {
-    let mut group = c.benchmark_group("Applicative order benchmark");
+fn bench_term<'a, M: Measurement>(group: &mut BenchmarkGroup<'a, M>, term_source: &str) {
+    bench_term_order(
+        group,
+        term_source,
+        "Normal order",
+        Term::reduce_step_normal_order,
+        BruijnLevelsTerm::reduce_step_normal_order,
+    );
 
-    bench_term(
-        &mut group,
-        "(ADD (ADD 2 5) (ADD 10 15))",
+    bench_term_order(
+        group,
+        term_source,
+        "Applicative order",
         Term::reduce_step_applicative_order,
         BruijnLevelsTerm::reduce_step_applicative_order,
     );
 }
 
-fn no_bench(c: &mut Criterion) {
-    let mut group = c.benchmark_group("Normal order benchmark");
+fn bench(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Term reductions");
 
-    bench_term(
-        &mut group,
-        "(ADD (ADD 2 5) (ADD 10 15))",
-        Term::reduce_step_normal_order,
-        BruijnLevelsTerm::reduce_step_normal_order,
-    );
+    bench_term(&mut group, "(ADD (ADD 200 500) (ADD 1000 1500))");
 }
 
-criterion_group!(benches, ao_bench, no_bench);
+criterion_group!(benches, bench);
 criterion_main!(benches);
